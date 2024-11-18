06ccc5e5539a0c577b6c2c62aedc1f09bd07c42a
lukajk1 <73144336+lukajk1@users.noreply.github.com>
2024-11-18 01:58:34 -0800

    tweak homepage to static image

--- js/shader.js
+++ js/shader.js
@@ -1,8 +1,8 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
-const width = 400.;
-const height = 400.;
+const width = 330.;
+const height = 330.;
 
 renderer.setSize(width, height);
 //renderer.setSize(window.innerWidth, window.innerHeight);
